6ababac937acae32418bcf3e0de67d45a876a8dc
nazat47 <nazat.mf@gmail.com>
2024-03-17 13:12:15 +0600

    updated mainpagev2

--- client/src/app/dua/[category]/page.jsx
+++ client/src/app/dua/[category]/page.jsx
@@ -1,12 +1,3 @@
-"use client";
-import { useEffect, useState, useRef } from "react";
-import { IoMdSearch } from "react-icons/io";
-import { MdOutlineSubdirectoryArrowRight } from "react-icons/md";
-import { IoLanguageOutline } from "react-icons/io5";
-import { RxDashboard } from "react-icons/rx";
-import { HiOutlineFolderOpen } from "react-icons/hi";
-import axios from "axios";
-import DuaCard from "./_components/DuaCard";
 import MainPage from "./_components/MainPage";
 
 const page = ({ params }) => {
